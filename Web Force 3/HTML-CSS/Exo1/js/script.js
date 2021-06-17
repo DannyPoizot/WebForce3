881363f8cc2574d3aa14af1684c2a7db6411416a
DannyPoizot <73651957+DannyPoizot@users.noreply.github.com>
2021-06-17 16:28:10 +0200

    Add files via upload

---
+++ Web Force 3/HTML-CSS/Exo1/js/script.js
@@ -0,0 +1,4 @@
+let year = new Date();
+let currentYear = year.getFullYear();
+
+document.getElementById("copyright").innerHTML = "© Copyright Danny Poizot " + currentYear + " Tous Droits Réservés.";
